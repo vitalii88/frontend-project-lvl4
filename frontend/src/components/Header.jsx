3468dd7c241468874b4060a7c947bf54b371b4ef
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-07-20 13:07:52 +0300

    change href to Link komponent

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import {Container, Navbar, Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
     <>
       <Navbar bg='light' className="shadow-sm">
         <Container>
-          <Navbar.Brand href="#home">My chat</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">My chat</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <Button variant="primary" href='/login'>Login</Button>
+            <Button as={Link} variant="primary" to='/login'>Login</Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
